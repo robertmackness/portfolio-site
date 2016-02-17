8e021afea116a1e1884bf33eba285a9b759fdfd6
robertmackness <robert.mackness.gbr@gmail.com>
2016-02-17 22:06:08 +0000

    Added main routes. Designed 'About' Page. 1. changed color scheme 2. changed responsive sizing of nav-bar 3. added main routes with ngRoute and $routeProvider 4. added main template for root route 5. added profile image for main template 6. added background image 7. added logos for main page

--- public/javascripts/portfolioApp.js
+++ public/javascripts/portfolioApp.js
@@ -1,2 +1,14 @@
 var portfolioApp = angular.module('portfolioApp', ['ngRoute', 'ngResource']);
 
+portfolioApp.config(function($routeProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: 'assets/templates/template-main.html',
+    })
+    .when('/cv', {
+      templateUrl: 'assets/templates/template-cv.html',
+    })
+    .when('/portfolio', {
+      templateUrl: 'assets/templates/template-portfolio.html',
+    })
+});

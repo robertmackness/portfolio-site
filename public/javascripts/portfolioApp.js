a7dc1391aeac725459e2dee747723f6300f0ad8c
robertmackness <robert.mackness.gbr@gmail.com>
2016-02-18 15:05:06 +0000

    Added Angular ngResource consumption of RESTful API w/Express 1. Added $resource factory to facilitate consumption of API    within portfolioApp.js 2. Added RESTful routes to /route/customer_route.js within Express App 3. Generated a test page at #/portfolio/customerserviceportal 4. Tested both index and single customer retrieval using parameters    /customers/:id

--- public/javascripts/portfolioApp.js
+++ public/javascripts/portfolioApp.js
@@ -1,5 +1,11 @@
+//################################
+// MODULE
+//################################
 var portfolioApp = angular.module('portfolioApp', ['ngRoute', 'ngResource']);
 
+//################################
+// ROUTES
+//################################
 portfolioApp.config(function($routeProvider){
   $routeProvider
     .when('/', {
@@ -11,4 +17,33 @@ portfolioApp.config(function($routeProvider){
     .when('/portfolio', {
       templateUrl: 'assets/templates/template-portfolio.html',
     })
+    .when('/portfolio/customerserviceportal', {
+      templateUrl: 'assets/templates/template-portfolio-customer-service-portfolio.html',
+    })
 });
+
+//################################
+// FACTORIES
+//################################
+portfolioApp.factory("CustomersAPI", function($resource) {
+  return $resource("/customers/:id", {},
+    {
+      'create':  { method: 'POST' },
+      'index':   { method: 'GET', isArray: true },
+      'show':    { method: 'GET', isArray: false },
+      'update':  { method: 'PUT' },
+      'destroy': { method: 'DELETE' }
+    }
+  );
+});
+
+//################################
+// CONTROLLERS
+//################################
+portfolioApp.controller('customerServicePortal', ['$scope', '$resource', 'CustomersAPI', function($scope, $resource, CustomersAPI){
+
+  $scope.customers = CustomersAPI.index();
+  console.log($scope.customers);
+
+}]);
+
--- routes/customer_router.js
+++ routes/customer_router.js
@@ -6,15 +6,36 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var customer = require(__dirname + '/../custom_modules/mongoose_schema_customer.js');
 
-/* GET users listing. */
-router.get('/all', function(req, res, next) {
+// GET
+router.get('', function(req, res, next) {
   // test - return first 100 users
   customer.find({}, function (err, customers) {
     if (err) throw err;
-    res.end("" + customers);
+    res.json(customers);
+    res.end();
   });
 });
 
+// GET 1 by ID
+router.get('/:id', function(req, res, next){
+  customer.find({_id: req.params.id}, function(err, customer){
+    if(err) throw err;
+    res.json(customer);
+    res.end();
+  })
+});
+// CREATE 1
+router.post('/', function(req, res, next){
+  
+});
+// UPDATE 1 by ID
+router.put('/:id', function(req, res, next){
+  
+});
+// DESTROY 1 by ID
+router.delete('/:id', function(req, res, next){
+  
+});
 
 //Export the router
 module.exports = router;

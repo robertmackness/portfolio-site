3cd10404cdf93ccb7b40fb3637f7abf9578c5696
robertmackness <robert.mackness.gbr@gmail.com>
2016-03-02 12:54:15 +0000

    Add updated CV and link on navbar

--- public/javascripts/portfolioApp.js
+++ public/javascripts/portfolioApp.js
@@ -11,9 +11,6 @@ portfolioApp.config(function($routeProvider){
     .when('/', {
       templateUrl: 'assets/templates/template-main.html',
     })
-    .when('/cv', {
-      templateUrl: 'assets/templates/template-cv.html',
-    })
     .when('/portfolio', {
       templateUrl: 'assets/templates/template-portfolio.html',
     })

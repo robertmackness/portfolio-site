d60536434439dd9d6c2f2ac8027accab7be3f391
robertmackness <robert.mackness.gbr@gmail.com>
2016-03-20 13:30:35 +0000

    Serve static images via Cloudinary CDN

--- blogPosts/mongoDBRegExpDraft.js
+++ blogPosts/mongoDBRegExpDraft.js
@@ -10,35 +10,70 @@ db.customers.find().forEach(
 );
 
 // common mistake: "I'll just use a case insensitive RegExp..."
-db.customers.find({first_name: /Stephen/i, last_name: /Fowlery/i, company: /Tanoodle/i}).explain("executionStats");
+db.customers.find({first_name: /a/i, last_name: /b/i, company: /c/i}).explain("executionStats");
+//time: 
+    "executionSuccess" : true,
+    "nReturned" : 941,
+    "executionTimeMillis" : 59,
+    "totalKeysExamined" : 0,
+    "totalDocsExamined" : 40000,
+    "executionStages" : {
+      "stage" : "COLLSCAN",
 
-// Add indexes - retarded
+
+
+//reason: This search is checking every single string fully. There is no indexing.
+
+// I'll add some indexes, that'll fix it
+db.customers.createIndex({first_name: 1});
+db.customers.createIndex({last_name: 1});
+db.customers.createIndex({company: 1});
+db.customers.find({first_name: /a/i, last_name: /b/i, company: /c/i}).explain("executionStats");
+//time:
+    "nReturned" : 941,
+    "executionTimeMillis" : 114,
+    "totalKeysExamined" : 40000,
+    "totalDocsExamined" : 10537,
+    "executionStages" : {
+      "stage" : "KEEP_MUTATIONS",
+
+
+//reason: MongoDB can't use indexes with RegExp unless anchored to the start. Also, it's having to check 3 separate indexes.
+
+// Ok I'll just index everything and use ^ anchors
 For queries starting with a first name:
-db.customers.addIndex(first_name: 1});
-db.customers.addIndex(first_name: 1, last_name: 1);
-db.customers.addIndex(first_name: 1, last_name: 1, company: 1);
-db.customers.addIndex(first_name: 1, company: 1, last_name: 1);
-
+db.customers.createIndex({first_name: 1});
+db.customers.createIndex({first_name: 1, last_name: 1);
+db.customers.createIndex({first_name: 1, last_name: 1, company: 1);
+db.customers.createIndex({first_name: 1, company: 1, last_name: 1);
 For queries starting with a last name:
-db.customers.addIndex(last_name: 1});
-db.customers.addIndex(last_name: 1, first_name: 1);
-db.customers.addIndex(last_name: 1, first_name: 1, company: 1);
-db.customers.addIndex(last_name: 1, company: 1, last_name: 1);
-
+db.customers.createIndex({last_name: 1});
+db.customers.createIndex({last_name: 1, first_name: 1);
+db.customers.createIndex({last_name: 1, first_name: 1, company: 1);
+db.customers.createIndex({last_name: 1, company: 1, last_name: 1);
 For queries starting with a company name:
-db.customers.addIndex(company: 1});
-db.customers.addIndex(company: 1, first_name: 1);
-db.customers.addIndex(company: 1, first_name: 1, last_name: 1);
-db.customers.addIndex(company: 1, last_name: 1, first_name: 1);
+db.customers.createIndex({company: 1});
+db.customers.createIndex({company: 1, first_name: 1);
+db.customers.createIndex({company: 1, first_name: 1, last_name: 1);
+db.customers.createIndex({company: 1, last_name: 1, first_name: 1);
+db.customers.find({first_name: /^a/, last_name: /^b/, company: /^c/}).explain("executionStats");
+//time: N/A 
+//reason:This is totally unneeded and causes extra overhead for processing creates, updates and queries. This will start to hurt later down the line.
 
-// Add indexes - smart
-db.customers.addIndex(first_name: 1, last_name: 1, company: 1);
-db.customers.addIndex(first_name: 1, company: 1, last_name: 1);
-db.customers.addIndex(last_name: 1, first_name: 1, company: 1);
-db.customers.addIndex(last_name: 1, company: 1, last_name: 1);
-db.customers.addIndex(company: 1, first_name: 1, last_name: 1);
-db.customers.addIndex(company: 1, last_name: 1, first_name: 1);
+// Compound indexes w/anchors and lower-case sanitisation on client - smart
+db.customers.createIndex({first_name: 1, last_name: 1, company: 1});
+db.customers.createIndex({first_name: 1, company: 1, last_name: 1});
+db.customers.createIndex({last_name: 1, first_name: 1, company: 1});
+db.customers.createIndex({last_name: 1, company: 1, last_name: 1});
+db.customers.createIndex({company: 1, first_name: 1, last_name: 1});
+db.customers.createIndex({company: 1, last_name: 1, first_name: 1});
+db.customers.find({first_name: /^a/, last_name: /^b/, company: /^c/}).explain("executionStats");
+//time:
+    "executionTimeMillis" : 5,
+    "totalKeysExamined" : 132,
+    "totalDocsExamined" : 19,
 
+//reason:
 
 // http://www.json-generator.com/ 10,000 users tool
 // remove comma after each object
